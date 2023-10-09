ba615c45d088ec3da40923cf84d3a646524344f3
Mehrdad Javan <mehrdad.javan@adapta.se>
2023-10-09 11:32:16 +0200

    define beans in app config

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -1,9 +1,37 @@
 package org.example.config;
 
+import org.example.dao.impl.TransactionDaoImpl;
+import org.example.dao.impl.WalletDaoImpl;
+import org.example.service.WalletManagement;
+import org.example.service.impl.TransactionManagementImpl;
+import org.example.service.impl.WalletManagementImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
 public class AppConfig {
+
+    /*@Bean
+    public TransactionDaoImpl transactionDao(){
+        return new TransactionDaoImpl();
+    }
+
+    @Bean
+    public WalletDaoImpl walletDao(){
+        return new WalletDaoImpl();
+    }
+
+    @Bean
+    public TransactionManagementImpl transactionManagement(){
+        return new TransactionManagementImpl(walletDao(), transactionDao());
+    }
+
+    @Bean
+    public WalletManagementImpl walletManagement(){
+        return new WalletManagementImpl(walletDao());
+    }*/
+
+
 }
